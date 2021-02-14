94af47abb87b79b49cdceb5a72c59b63f7646fa0
ATodorovic275 <atodorovic197@gmail.com>
2021-02-14 11:15:02 +0100

    update home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,19 +11,13 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // $users = User::with(['projects'])->get();
-        // $users = User::with(['projects'])->where('users.id', '8')->get();
-
         $users = DB::table('users')
             ->select(['users.name as user', 'users.email', 'projects.name as project', 'project_assignments.id as pAssignment'])
             ->join('project_assignments', 'users.id', 'project_assignments.user_id')
             ->join('projects', 'project_assignments.project_id', 'projects.id')
-            // ->join('user_tasks', 'project_assignments.user_id', 'user_tasks.id')
-            // ->join('tasks', 'user_tasks.task_id', 'users.id')
-            // ->where('users.id', '8')
             ->get();
 
-        // dd($users);
+
         foreach ($users as $user) {
             $idProjectAssignment = $user->pAssignment;
             $tasks = DB::table('tasks')
@@ -32,12 +26,8 @@ class HomeController extends Controller
                 ->join('project_assignments', 'user_tasks.project_assignment_id', 'project_assignments.id')
                 ->where('project_assignments.id', $idProjectAssignment)
                 ->get();
-            // dd($tasks);
             $user->tasks = $tasks;
-            // dd($idProjectAssignment);
-            // $tasks = DB
         }
-        // dd($users);
 
         return view('home', ['users' => $users]);
     }
